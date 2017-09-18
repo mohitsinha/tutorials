64aa41c3f97ffc9eb1bbdd8aa0efb04290c66189
Mohit Sinha <mohitsinha251@gmail.com>
2017-09-18 16:31:54 +0530

    Initial commit - Removed junit 4.12 and replaced it with Junit jupiter api

---
+++ junit5-spring-boot/src/main/java/com/example/junit5springboot/Junit5SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.junit5springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5SpringBootApplication.class, args);
+	}
+}
---
+++ junit5-spring-boot/src/test/java/com/example/junit5springboot/Junit5SpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.junit5springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class Junit5SpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
