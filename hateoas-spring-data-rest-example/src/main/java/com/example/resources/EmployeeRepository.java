c580a63409fb5cad3a98d874fad3e65f54a69b5c
Mohit Sinha <mohitsinha251@gmail.com>
2017-10-11 18:39:24 +0530

    Deleted Spring Data Rest Mongo

--- hateoas-spring-data-rest-example/src/main/java/com/example/resources/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.resources;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface EmployeeRepository extends MongoRepository<Employee, UUID> {
-}
