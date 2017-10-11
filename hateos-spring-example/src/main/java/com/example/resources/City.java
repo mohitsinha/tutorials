eaabc2e54ea9a1934690270083dd75f9c9b456c9
Mohit Sinha <mohitsinha251@gmail.com>
2017-10-11 14:42:52 +0530

    Removed unnecessary dependencies

--- hateos-spring-example/src/main/java/com/example/resources/City.java
+++ hateos-spring-example/src/main/java/com/example/resources/City.java
@@ -6,10 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data
 @Entity
